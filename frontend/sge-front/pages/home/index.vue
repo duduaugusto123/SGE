0bf839159ab887f6cc91b1e815befe0c88eb233f
Desenvolvimento <dsadm@educ501.local>
2024-03-19 20:09:08 -0300

    adicinado login alternativo com then/catch

--- frontend/sge-front/pages/home/index.vue
+++ frontend/sge-front/pages/home/index.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
+const { signOut } = useAuth();
 definePageMeta({
     middleware: 'auth'
 })
+
+const submitLogout = ()=> {
+    signOut({redirect: false}).then(()=>{
+        console.log("successfully performed the logout");
+        navigateTo("/")
+    }).catch((error)=>{
+        console.log("error when trying logout ", error);
+    })
+}
 </script>
 
 <template>
     <div>
         <h1>Minha Tela Home!!!</h1>    
+        <button @click="submitLogout" class="customButton">SAIR</button>
     </div>
 </template>
 
