d80cdf253864414c12c90b390b4c3672518e5d90
Desenvolvimento <dsadm@educ501.local>
2024-04-11 21:10:28 -0300

    added static files

--- backend/sge/settings.py
+++ backend/sge/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 production_server = 'https://sge-senai-mange.up.railway.app'
 
